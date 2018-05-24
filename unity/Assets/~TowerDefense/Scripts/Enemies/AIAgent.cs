12671c167e19dace47125a8fbf78a97d58feaa9d
oliver.worthington1 <oliver.worthington1@600900B211DE409>
2018-05-24 15:40:30 +1000

    Camera Orbit

---
+++ unity/Assets/~TowerDefense/Scripts/Enemies/AIAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        private void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (target)
+            {
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
+
+
