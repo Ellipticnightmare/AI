1425042b9fe27802a5a48f8bd21e6f846572ea2f
ellipticnightmare <IndieDevSA@gmail.com>
2018-05-31 15:52:14 +1000

    enemyspawner finished

--- unity/Assets/~TowerDefense/Scripts/Enemies/Enemy.cs
+++ unity/Assets/~TowerDefense/Scripts/Enemies/Enemy.cs
@@ -1,12 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TowerDefense
 {
     public class Enemy : MonoBehaviour
     {
-        public float health = 100f;
+        public float maxHealth = 100f;
+        [Header("UI")]
+        public GameObject healthbarUI;
+        public Vector2 healthBarOffset = new Vector2(0f, 5f);
+
+        private Slider healthSlider;
+        private float health = 100f;
+
+        void Start()
+        {
+            health = maxHealth;
+        }
+
+        Vector3 GetHealthBarPos()
+        {
+            Camera cam = Camera.main;
+            Vector2 position = cam.WorldToScreenPoint(transform.position);
+            return position + healthBarOffset;
+        }
+
+        void Update()
+        {
+            if (healthSlider)
+            {
+                healthSlider.transform.position = GetHealthBarPos();
+            }
+        }
+
+        public void SpawnHealthBar(Transform parent)
+        {
+            GameObject clone = Instantiate(healthbarUI, 
+                                            GetHealthBarPos(), 
+                                            Quaternion.identity,
+                                            parent);
+
+            healthSlider = clone.GetComponent<Slider>();
+        }
 
         public void DealDamage(float damage)
         {
