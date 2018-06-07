997cd42fd8e6a2b8d2653637758603e1e75f31b1
ellipticnightmare <IndieDevSA@gmail.com>
2018-06-07 15:52:48 +1000

    Removed Library & other garbage Tower aiming

---
+++ Assets/~TowerDefense/Scripts/Towers/Cannon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Cannon : Tower
+    {
+        public Transform barrel;
+        public float lineDelay = .2f;
+
+        private LineRenderer line;
+
+        // Use this for initialization
+        void Awake()
+        {
+            line = GetComponent<LineRenderer>();   
+        }
+
+        public override void Aim(Enemy e)
+        {
+            barrel.LookAt(e.transform);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, barrel.position);
+            line.SetPosition(2, e.transform.position);
+        }
+
+        public override void Attack(Enemy e)
+        {
+
+        }
+    }
+}
+
---
+++ Assets/~TowerDefense/Scripts/Towers/Tower.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Tower : MonoBehaviour
+    {
+        public float damage = 10f;
+        public float attackDelay = 1f;
+
+        protected Enemy currentEnemy;
+
+        private float attackTimer = 0f;
+
+        public virtual void PrintName()
+        {
+            print("Tower");
+        }
+
+        public virtual void Aim(Enemy e) { }
+        public virtual void Attack(Enemy e) { }
+
+        protected virtual void Update()
+        {
+            attackTimer += Time.deltaTime;
+            if (currentEnemy)
+            {
+                Aim(currentEnemy);
+                if(attackTimer >= attackDelay)
+                {
+                    Attack(currentEnemy);
+                    attackTimer = 0f;
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Enemy hitEnemy = other.GetComponent<Enemy>();
+            if(hitEnemy && currentEnemy == null)
+            {
+                currentEnemy = hitEnemy;
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            Enemy hitEnemy = other.GetComponent<Enemy>();
+            if(hitEnemy && currentEnemy == null)
+            {
+                currentEnemy = hitEnemy;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Enemy hitEnemy = other.GetComponent<Enemy>();
+            if(hitEnemy && currentEnemy == hitEnemy)
+            {
+                currentEnemy = null;
+            }
+        }
+    }
+}
+
