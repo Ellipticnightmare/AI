62386e884cfe96f77b2fe8d4fd7ec64407e8ae35
ellipticnightmare <IndieDevSA@gmail.com>
2018-06-21 14:56:35 +1000

    Stuff

---
+++ Assets/~Billiards/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Billiards
+{
+    public class Ball : MonoBehaviour
+    {
+        public float stopSpeed = 0.2f;
+
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            Vector3 vel = rigid.velocity;
+
+            // Check if velocity is going up
+            if (vel.y > 0)
+            {
+                vel.y = 0;
+            }
+
+            if (vel.magnitude < stopSpeed)
+            {
+                vel = Vector3.zero;
+            }
+
+            rigid.velocity = vel;
+        }
+
+        public void Hit(Vector3 dir, float impactForce)
+        {
+            rigid.AddForce(dir * impactForce, ForceMode.Impulse);
+        }
+    }
+}
+
+
