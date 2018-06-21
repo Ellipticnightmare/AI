e6e193b377f485593d3338ec74f45424fc84b13a
ellipticnightmare <IndieDevSA@gmail.com>
2018-06-21 15:50:01 +1000

    All finished

---
+++ Assets/~Billiards/Scripts/Cue.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Billiards
+{
+    public class Cue : MonoBehaviour
+    {
+        public Ball targetBall;
+        public float minPower = 0f;
+        public float maxPower = 20f;
+        public float maxDistance = 5f;
+
+        private float hitPower;
+        private Vector3 aimDirection;
+        private Vector3 prevMousePos;
+        private Ray mouseRay;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(mouseRay.origin, mouseRay.origin + mouseRay.direction * 1000f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(targetBall.transform.position, targetBall.transform.position + aimDirection * hitPower);
+        }
+
+        void Aim()
+        {
+            mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(mouseRay, out hit))
+            {
+                Vector3 dir = transform.position - hit.point;
+                float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+                transform.position = targetBall.transform.position;
+            }
+        }
+
+        public void Deactivate()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Activate()
+        {
+            Aim();
+            gameObject.SetActive(true);
+        }
+
+        void Drag()
+        {
+            Vector3 targetPos = targetBall.transform.position;
+            Vector3 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float distance = Vector3.Distance(prevMousePos, currMousePos);
+            distance = Mathf.Clamp(distance, 0, maxDistance);
+            float distPercentage = distance / maxDistance;
+            hitPower = Mathf.Lerp(minPower, maxPower, distPercentage);
+            transform.position = targetPos - transform.forward * distance;
+            aimDirection = (targetPos - transform.position).normalized;
+        }
+
+        void Fire()
+        {
+            targetBall.Hit(aimDirection, hitPower);
+            Deactivate();
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                prevMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                Drag();
+            }
+            else
+            {
+                Aim();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Fire();
+            }
+        }
+    }
+}
+
+
---
+++ Assets/~TowerDefense/Scripts/Selection/Selector.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Selector : MonoBehaviour
+    {
+        public GameObject[] prefabs;
+        public LayerMask layerMask;
+
+        private GameObject[] instances;
+        private Transform tower, hologram;
+        private int currentTower;
+
+        public void SelectTower(int index)
+        {
+            if (index < 0 || index > prefabs.Length)
+                return;
+
+            currentTower = index;
+        }
+
+        void GenerateInstances()
+        {
+            GameObject instance = instances[currentTower];
+            if(instance == null)
+            {
+                instance = Instantiate(prefabs[currentTower]);
+                instance.transform.SetParent(transform);
+
+                tower = instance.transform.Find("Tower");
+                hologram = instance.transform.Find("Hologram");
+
+                tower.gameObject.SetActive(false);
+                hologram.gameObject.SetActive(false);
+
+                instances[currentTower] = instance;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            instances = new GameObject[prefabs.Length];
+            SelectTower(0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GenerateInstances();
+
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(mouseRay, out hit, 1000f,layerMask, QueryTriggerInteraction.Ignore))
+            {
+                Placeable p = hit.transform.GetComponent<Placeable>();
+
+                if(p && p.isPlaced == false)
+                {
+                    GameObject gHologram = hologram.gameObject;
+                    GameObject gTower = tower.gameObject;
+
+                    if(gHologram.activeSelf == false)
+                    {
+                        gHologram.SetActive(true);
+                    }
+
+                    GameObject instance = instances[currentTower];
+
+                    instance.transform.position = p.transform.position;
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        p.isPlaced = true;
+
+                        gHologram.SetActive(false);
+                        gTower.SetActive(true);
+
+                        instance.transform.SetParent(p.transform);
+                        instances[currentTower] = null;
+                    }
+                }
+            }
+        }
+    }
+}
+
